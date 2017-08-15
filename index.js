f60c4b3f53b61a173e42f4e9701569623cb69d39
Steven Xiao <stevenwxiao@gmail.com>
2017-08-14 20:02:35 -0700

    store user profile in mongo db

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/user.js')
 require('./services/passport')
+
+const keys = require('./config/keys')
 const authRoutes = require('./routes/authRoutes')
+
+mongoose.connect(keys.mongoURI);
 //Generate a express application
 const app = express();
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 6 * 60 * 1000,
+		keys: [keys.cookieKey]
+
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session())
 
 //clientId 360784595279-ej1hr5gc4utpglq16u0kp3qq3l5em4aa.apps.googleusercontent.com
 //clientSecret g6VdBqmeS829KrUqlLfhOiMU
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,21 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys.js')
 const passport = require('passport');
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+	User.findById(id)
+		.then(user => {
+			done(null, user);
+		}) 
+
+})
+
 passport.use(
 	new GoogleStrategy({
 		clientID: keys.googleClientID,
@@ -8,10 +23,17 @@ passport.use(
 		callbackURL: '/auth/google/callback'
 	}, 
 	(accessToken, refreshToken, profile, done) =>{
-		console.log("accessToken", accessToken);
-		console.log("refreshToken", refreshToken);
-		console.log("profile",profile);
-		console.log("done:",done);
+		User.findOne({googleId: profile.id})
+			.then((existingUser) => {
+				if (existingUser) {
+					// we already have a record of given profile Id
+					done(null, existingUser);
+				} else {
+					// we don't have a user record
+					new User({googleId: profile.id }).save()
+						.then(user => done(null, user)); 
 
+				}
+			})
 	})
 );
